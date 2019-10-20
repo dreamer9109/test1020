254a5f6dc1a5d6563c1bd4c19b91dfc10d5a12ce
dreamer9109 <1156464040@qq.com>
2019-10-20 17:14:00 +0800

    提交

--- wangzhijiang/src/main/java/com/xu/demo/HelloWorld1.java
+++ wangzhijiang/src/main/java/com/xu/demo/HelloWorld1.java
@@ -1,6 +1,8 @@
+package com.xu.demo;
+
 /**
  * @author xuqiufeng
- * @date 2019/10/20 4:50 PM
+ * @ 2019/10/20 4:50 PM
  */
 public class HelloWorld1 {
     public static void main(String[] args) {
