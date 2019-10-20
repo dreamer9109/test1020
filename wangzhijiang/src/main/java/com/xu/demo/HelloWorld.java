f1c440483c9b092a5795243ded484f670b8a6bdf
dreamer9109 <1156464040@qq.com>
2019-10-20 16:12:19 +0800

    增加模块一

---
+++ wangzhijiang/src/main/java/com/xu/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xu.demo;
+
+/**
+ * @author xuqiufeng
+ * @date 2019/10/20 4:10 PM
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
